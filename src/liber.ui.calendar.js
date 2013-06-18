810f9336c0c08f5b8e8670ac2bcca25696a9be85
soyoes <soyoes@gmail.com>
2013-06-19 08:36:37 +0900

    fix bugs in ui.calendar

--- src/liber.ui.calendar.js
+++ src/liber.ui.calendar.js
@@ -2,13 +2,21 @@ $utils.package("liber.ui.calendar");
 
 
 var Calendar = function(attrs,onclick){
-	d =new Date();
+	var d =new Date();
 	this.year = d.getFullYear();
 	this.month = d.getMonth();
 	this.wday = d.getDay();
 	this.day = d.getDate();
 	this.opts = attrs;
-	this.onclick = onclick;
+	this._onclick = onclick;
+	this.onclick = function(e){
+		if(!_this.onclick)
+			return;
+		e=e||window.event;
+		var cell=e.target||e.srcElement;
+		var date = cell.attr("date");
+		_this._onclick(date);
+	};
 
 	this.dom = undefined;
 
@@ -17,49 +25,47 @@ var Calendar = function(attrs,onclick){
 	this.daysOfMonth = function(y,m){return 32 - new Date(y, m, 32).getDate();};	
 
 	this.daysOfLastMonth = function(y,m){
-		_1st_wday =  new Date(y,m,1).getDay();
+		var _1st_wday =  new Date(y,m,1).getDay();
 		if(_1st_wday==0)
 			return [];
-		last_m = m==0?11:m-1;
-		last_y = m==0?y-1:y;
-		last_m_days = this.daysOfMonth(last_y, last_m);
-		res = [];
-		for(i = last_m_days-_1st_wday+1; i<=last_m_days;i++)
+		var last_m = m==0?11:m-1,
+			last_y = m==0?y-1:y,
+			last_m_days = this.daysOfMonth(last_y, last_m),
+			res = [];
+		for(var i = last_m_days-_1st_wday+1; i<=last_m_days;i++)
 			res.push(i);
 		return res;
 	};
 
 	this.calDays = function(y,m){
-		days = this.daysOfLastMonth(y,m);
-
-		currentDays = this.daysOfMonth(y,m);
-		for(i=1;i<=currentDays;i++){
+		var days = this.daysOfLastMonth(y,m);
+		var currentDays = this.daysOfMonth(y,m);
+		for(var i=1;i<=currentDays;i++){
 			days.push(i);
 		}
-		lastwday = days.length%7;
-
+		var lastwday = days.length%7;
 		if(lastwday>0){
-			for(i=1;i<=7-lastwday;i++)
+			for(var i=1;i<=7-lastwday;i++)
 				days.push(i);	
 		}
 		return days;
 	};
 
 	this.nextMonth = function(e){
-		cal = $id("calendar");
+		var cal = $id("calendar"),
+			m = _this.month,
+			y = _this.year;
 		cal.innerHTML="";
-		m = _this.month;
-		y = _this.year;
 		_this.month = m==11?0:m+1;
 		_this.year = m==11?y+1:y;
 		_this.draw();
 		e.preventDefault();
 	};
 	this.prevMonth = function(e){
-		cal = $id("calendar");
+		var cal = $id("calendar"),
+			m = _this.month,
+			y = _this.year;
 		cal.innerHTML="";
-		m = _this.month;
-		y = _this.year;
 		_this.month = m==0?11:m-1;
 		_this.year = m==0?y-1:y;
 		_this.draw();
@@ -68,9 +74,9 @@ var Calendar = function(attrs,onclick){
 
 
 	this.draw = function(){
-		attrs = this.opts?this.opts:{};
+		var attrs = this.opts?this.opts:{};
 		attrs["id"] = "calendar";
-		frame = $id("calendar");
+		var frame = $id("calendar");
 		if(!frame){
 			frame = $div(attrs);
 		}
@@ -78,34 +84,34 @@ var Calendar = function(attrs,onclick){
 		_this.dom = frame;
 		/* year - month row */
 
-		monstrs = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		title = $div([
+		var monstrs = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+			wdays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+			days = _this.calDays(_this.year, _this.month),
+			row = null,
+			thismon = false;
+		
+		$div([
 			$div([
-				$a({href:"#",html:"◀",onclick:_this.prevMonth}),
-				$label({html:monstrs[_this.month]+", "+this.year}),
-				$a({href:"#",html:"▶",onclick:_this.nextMonth}),
+				$a({href:"#",html:"<",onclick:_this.prevMonth}),
+				$label({html:monstrs[_this.month]+", "+_this.year}),
+				$a({href:"#",html:">",onclick:_this.nextMonth}),
 			]).css("minWidth",'100px').attr({"class":"titleWrapper"}),
-		],frame).attr({"class":"title"}),
-				
-		wdays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-		subtitle = $ul((function(){
-			lis = [];
-			for(wd in wdays)
+		],frame).attr({"class":"title"});
+		$ul((function(){
+			var lis = [];
+			for(var wd in wdays)
 				lis.push($li(wdays[wd]));
 			return lis;
-		})(),frame).attr({"class":"subtitle"}),
-
-		days = _this.calDays(_this.year, _this.month);
-		row = null;
-		thismon = false;	
-		for(d in days){
+		})(),frame).attr({"class":"subtitle"});
+				
+		for(var d in days){
 			if(d%7==0) row = $ul({"class":"days"},frame);
-			day = days[d];
+			var day = days[d];
 			if(!thismon && day==1) thismon = true;
 			else if(thismon && day==1) thismon = false;
-			cls = !thismon?(d<7?"other lastmonth":"other nextmonth"):((d%7==0)?"sun":(d%7==6?"sat":""));
-			cell = $li({html:day,"class":cls,"date":[_this.year,_this.month+1,day].join("-")},row);
-			cell.onclick = _this.onclick;
+			var cls = !thismon?(d<7?"other lastmonth":"other nextmonth"):((d%7==0)?"sun":(d%7==6?"sat":""));
+			$li({html:day,"class":cls,"date":[_this.year,(_this.month+1)<10?"0"+(_this.month+1):_this.month+1,
+					day<10?"0"+day:day].join("-")},row).bind('click',_this.onclick);
 		}
 	};
 };
